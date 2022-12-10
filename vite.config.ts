25c2e250ecd75354780b72a7078a423b50a73989
nekusu <nekusu.dev@gmail.com>
2022-12-10 17:34:27 -0300

    Fix github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import env from 'vite-plugin-env-compatible';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: '/administration-panel/',
   plugins: [react(), env({ prefix: 'REACT_APP' }), tsConfigPaths()],
   build: { outDir: 'build' },
 });
