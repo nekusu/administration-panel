bd4b00a212849bf25263b66eea9dd59fcf72e75e
nekusu <nekusu.dev@gmail.com>
2022-12-11 21:42:52 -0300

    Edit Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import env from 'vite-plugin-env-compatible';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: '/administration-panel/',
-  plugins: [react(), env({ prefix: 'REACT_APP' }), tsConfigPaths()],
+  plugins: [
+    react(),
+    env({ prefix: 'REACT_APP', mountedPath: 'import.meta.env' }),
+    tsConfigPaths(),
+    checker({ typescript: true }),
+  ],
   build: { outDir: 'build' },
 });
